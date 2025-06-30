9c0babd9c357d4cdd81c0d733407d46e17cb6cef
Ofer Aharonson <ofer.aharonson@gmail.com>
2025-07-01 00:04:06 +0300

    Delete Start-MusiBot.ps1

--- Start-MusiBot.ps1
+++
@@ -1,5 +0,0 @@
-# PowerShell script to start MusiBot in a new window
-$botPath = "C:\Code\MusiBot"
-$nodeCmd = "node index.js"
-
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd `"$botPath`"; $nodeCmd"
